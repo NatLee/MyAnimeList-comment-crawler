e614968f19b723ee95fdafc1fda2a35992a5a9cf
NatLee <natlee.work@gmail.com>
2023-06-03 03:35:01 +0800

    [update] fix URLs & reactions in review block

--- review_spider.py
+++ review_spider.py
@@ -1,5 +1,5 @@
+import json
 import scrapy
-from tqdm import tqdm
 from loguru import logger
 from utils.dbUtil import AnimeAccess, Database
 
@@ -7,7 +7,7 @@ class ReviewSpider(scrapy.Spider):
     name = 'review_spider'
     allowed_domains = ['myanimelist.net']
     custom_settings = {
-        'DOWNLOAD_DELAY': 10,
+        'DOWNLOAD_DELAY': 7,
         #'RETRY_HTTP_CODES': [429],
         #'RETRY_TIMES': 10
     }
@@ -20,15 +20,17 @@ class ReviewSpider(scrapy.Spider):
     def __init__(self):
         self.db = Database()
         self.anime_access = AnimeAccess(self.db)
-        self.urls = self.anime_access.get_all_work_urls()
+        self.urls = iter(self.anime_access.get_all_work_urls())  # Turn urls into an iterator
 
     def start_requests(self):
-        self.urls = iter(self.anime_access.get_all_work_urls())  # Turn urls into an iterator
         url = next(self.urls)  # Get first url
         base_url = f"{url}/reviews?sort=newest&filter_check=&filter_hide=&preliminary=on&spoiler=on&p="
         yield scrapy.Request(url=base_url+'1', callback=self.parse, meta={'base_url': base_url, 'page_number': 2, 'work_url': url})
 
     def parse(self, response):
+
+        reviews = []
+
         url = response.url
         work_id = url.split('/')[4]
         work_name = url.split('/')[5]
@@ -36,41 +38,29 @@ class ReviewSpider(scrapy.Spider):
 
         review_blocks = response.css('div.review-element')
 
-        if not review_blocks:
-            logger.warning(f"[{work_id}][{work_name}] No review blocks found on page")
-
-            # Proceed to next work
-            try:
-                next_work_url = next(self.urls)
-                logger.info(f'Go to next URL -> {next_work_url}')
-                base_url = f'{next_work_url}/reviews?sort=newest&filter_check=&filter_hide=&preliminary=on&spoiler=on&p='
-                yield scrapy.Request(url=base_url+'1', callback=self.parse, meta={'base_url': base_url, 'page_number': 2, 'work_url': next_work_url})
-            except StopIteration:  # No more urls to process
-                return
-
-        reviews = []
-
-        for review in tqdm(review_blocks):
-            reactions = review.css('.btn-reaction .num::text').getall()
-            # Initialize dictionary with all reaction types
-            reactions = {
-                'nice': '0',
-                'loveIt': '0',
-                'funny': '0',
-                'confusing': '0',
-                'informative': '0',
-                'wellWritten': '0',
-                'creative': '0'
-            }
-            reaction_blocks = review.css('.btn-reaction')
-            for reaction in reaction_blocks:
-                reaction_type = reaction.css('::attr(title)').get()
-                reaction_count = reaction.css('.num::text').get()
-                # Only update the reactions that exist in the review
-                if reaction_type in reactions:
-                    reactions[reaction_type] = reaction_count
-
+        for review in review_blocks:
             review_id = review.css('div.open a::attr(href)').re_first(r'\d+')
+
+            # Extract the 'data-reactions' attribute and convert it to Python dictionary
+            review_attrib = review.attrib
+            reactions_dict = review_attrib['data-reactions']
+            if reactions_dict != '':
+                reactions_data = json.loads(reactions_dict)
+            else:
+                reactions_data = {"icon":[],"num":0,"count":["0","0","0","0","0","0","0"]}
+
+            # Mapping the reaction types to the indexes in the icon array from data-reactions
+            reaction_type_map = [
+                'nice',
+                'loveIt',
+                'funny',
+                'confusing',
+                'informative',
+                'wellWritten',
+                'creative'
+            ]
+            reactions = {r:c for r,c in zip(reaction_type_map, reactions_data['count'])}
+            logger.debug(f'[W{work_id}][R{review_id}] {reactions}')
 
             review_data = {
                 'workId': work_id,
@@ -88,6 +78,21 @@ class ReviewSpider(scrapy.Spider):
             }
             review_data.update(reactions)
             reviews.append(review_data)
+
+        # Check for "More Reviews" link existence
+        next_page_exists = response.css('a.ga-click[data-ga-click-type="review-more-reviews"]::attr(href)').get()
+
+        if not next_page_exists:
+            logger.warning(f"[{work_id}][{work_name}] No next page")
+
+            # Proceed to next work
+            try:
+                next_work_url = next(self.urls)
+                logger.info(f'Go to next URL -> {next_work_url}')
+                base_url = f'{next_work_url}/reviews?sort=newest&filter_check=&filter_hide=&preliminary=on&spoiler=on&p='
+                yield scrapy.Request(url=base_url+'1', callback=self.parse, meta={'base_url': base_url, 'page_number': 2, 'work_url': next_work_url})
+            except StopIteration:  # No more urls to process
+                return
 
         logger.info(f"[{work_id}][{work_name}] Pushing {len(reviews)} reviews to the database")
         self.anime_access.push_reviews_to_database(reviews)
